824dacc489557022897d1e36a6ae7d2f2710b145
ALFinternet <ALFinternet@gmail.com>
2020-05-31 09:07:42 -0700

    more cleanup

--- prepare-centos-template.sh
+++ prepare-centos-template.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # install items
+# might need to install this manually to download this file from github:
+yum install -y git
+
 yum install -y open-vm-tools epel-release nano haveged nfs-utils yum-utils
 yum install -y bind-utils whois wget mlocate vim-enhanced cups net-snmp net-snmp-utils net-tools
 
@@ -12,10 +15,13 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 #stop logging services
 /sbin/service rsyslog stop
 /sbin/service auditd stop
+
 #remove old kernels
 /bin/package-cleanup –oldkernels –count=1
+
 #clean yum cache
 /usr/bin/yum clean all
+
 #force logrotate to shrink logspace and remove old logs as well as truncate logs
 /usr/sbin/logrotate -f /etc/logrotate.conf
 /bin/rm -f /var/log/*-???????? /var/log/*.gz
@@ -25,10 +31,14 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 /bin/cat /dev/null > /var/log/wtmp
 /bin/cat /dev/null > /var/log/lastlog
 /bin/cat /dev/null > /var/log/grubby
+
 #remove udev hardware rules
 /bin/rm -f /etc/udev/rules.d/70*
+
 #remove uuid from ifcfg scripts
-/bin/sed –i”.bak” ‘/UUID/d’ /etc/sysconfig/network-scripts/ifcfg-ens192
+#this doesn't work
+#/bin/sed –i ".bak" '/UUID/d' /etc/sysconfig/network-scripts/ifcfg-ens192
+
 #remove SSH host keys
 /bin/rm -f /etc/ssh/*key*
 #remove root users shell history
@@ -36,6 +46,12 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 unset HISTFILE
 #remove root users SSH history
 /bin/rm -rf ~root/.ssh/
+
+# reset machine-id by deleting the file & then creating a blank place holder
+# files: /etc/machine-id
+# ALFinternet 2020-04-16
+/bin/rm -f /etc/machine-id
+/bin/touch /etc/machine-id
 
 # run these 2 commands afterwards:
 # history –c
