f5e9f6ae026aa133ce7f887455c6334e0dab11a2
ALFinternet <ALFinternet@gmail.com>
2020-05-31 07:27:09 -0700

    add centOS template

--- docker-install.sh
+++ docker-install.sh
@@ -34,4 +34,22 @@ usermod -aG docker alf
 
 #systemctl enable docker
 #all remote connections
-sudo sed -i 's/containerd.sock/containerd.sock -H=tcp:\/\/0.0.0.0:2375/g' /lib/systemd/system/docker.service
+#sudo nano /etc/docker/daemon.json
+#sudo sed -i 's/containerd.sock/containerd.sock -H=tcp:\/\/0.0.0.0:2375/g' /lib/systemd/system/docker.service
+
+
+# https://nickjanetakis.com/blog/docker-tip-73-connecting-to-a-remote-docker-daemon
+# Create the directory to store the configuration file.
+sudo mkdir -p /etc/systemd/system/docker.service.d
+# Create a new file to store the daemon options.
+sudo nano /etc/systemd/system/docker.service.d/options.conf
+# Now make it look like this and save the file when you're done:
+cat << 'EOL' | sudo tee /etc/systemd/system/docker.service.d/options.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H unix:// -H tcp://0.0.0.0:2375
+EOL
+# Reload the systemd daemon.
+sudo systemctl daemon-reload
+# Restart Docker.
+sudo systemctl restart docker
