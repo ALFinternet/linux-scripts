ca52c043caf73797fdb89f31435ab8ba94686e84
ALFinternet <ALFinternet@gmail.com>
2023-07-08 23:32:07 -0700

    add docker

--- prepare-ubuntu-template.sh
+++ prepare-ubuntu-template.sh
@@ -40,6 +40,28 @@ apt install -y haveged ntp nfs-common net-tools cifs-utils htop parted tmux p7zi
 timedatectl set-timezone America/Los_Angeles
 
 
+# install docker because we always use it
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# end DOCKER
+
+
 #Stop services for cleanup
 service rsyslog stop
 
@@ -114,7 +136,6 @@ rm /etc/machine-id
 rm /var/lib/dbus/machine-id
 truncate -s 0 /var/lib/dbus/machine-id
 ln -s /var/lib/dbus/machine-id /etc/machine-id
-
 
 #cleanup apt
 apt clean
